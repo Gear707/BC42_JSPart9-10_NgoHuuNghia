85b0716d1231a408d9e4755996e6f0f8b81c230d
Nghia <nghia3977@gmail.com>
2023-01-16 09:53:29 +0700

    JS Part 9-10

--- js/main.js
+++ js/main.js
@@ -1,105 +1,155 @@
-queSel = (selector) => document.querySelector(selector);
-
-getID = (ID) => document.getElementById(ID);
+// Tạo mảng chứa danh sách nhân viên
+const staffList = [];
 
 // Thêm mới nhân viên
-queSel('#btnThemNV').addEventListener('click', () => {
+getEle('#btnThemNV').addEventListener('click', () => {
     // input: dữ liệu đầu vào của nhân viên
-    let account = queSel('#tknv').value; // input tài khoản
+    let account = String(getEle('#tknv').value); // input tài khoản
     let checkAcc = validateAcc(account); // kiểm tra input tài khoản
+    let checkLength = validateLength(account, 4, 6); // kiểm tra độ dài input tài khoản
 
-    let fullName = queSel('#name').value; // input họ tên
+    let fullName = getEle('#name').value; // input họ tên
     let checkName = validateName(fullName); // kiêm tra input họ tên
 
-    let email = queSel('#email').value; // input địa chỉ email
+    let email = getEle('#email').value; // input địa chỉ email
     let checkEmail = validateEmail(email); // kiểm tra input địa chỉ email
 
-    let password = queSel('#password').value;
-    let date = queSel('#datepicker').value;
-    let baseSalary = +queSel('#luongCB').value;
-    let position = queSel('#chucvu').value;
-    let hours = +queSel('#gioLam').value;
+    let password = getEle('#password').value;
+    let date = getEle('#datepicker').value;
+    let baseSalary = +getEle('#luongCB').value;
+    let position = getEle('#chucvu').value;
+    let hours = +getEle('#gioLam').value;
 
-    // tạo biến đối tượng cho lớp đối tượng StaffInfo, tương ứng là các tham số truyền vào từ input
-    let staff = new StaffInfo(account, fullName, email, password, date, baseSalary, position, hours);
+    // progress
+    // tạo object nhân viên
+    const staff = new StaffInfo(
+        account,
+        fullName,
+        email,
+        password,
+        date,
+        baseSalary,
+        position,
+        hours
+    );
 
-    // progress: tạo biến chứa value mà user đã nhập cho mỗi thuộc tính trong lớp đối tượng nhân viên
-    let tdAccount = '<td>' + staff.account + '</td>';
-    let tdFullName = '<td>' + staff.fullName + '</td>';
-    let tdEmail = '<td>' + staff.email + '</td>';
-    let tdDate = '<td>' + staff.date + '</td>';
-    let tdPosition = '<td>' + staff.position + '</td>';
-    let tdTotalSalary = '<td>' + staff.totalSalary() + '</td>';
-    let tdCategory = '<td>' + staff.category() + '</td>';
-    let tdDelete = '<td>' + '<button type="button" class="btn btn-danger ml-1" id="btnDelete">Delete</button>' + '</td>';
+    // thêm object nhân viên vào mảng staffList
+    staffList.push(staff);
 
-    // tạo biến chứa 1 hàng dữ liệu tương ứng mà user đã nhập
-    let tr = '<tr>' + tdAccount + tdFullName + tdEmail + tdDate + tdPosition + tdTotalSalary + tdCategory + tdDelete + '</tr>';
-
-    // output: hiển thị dữ liệu đã nhập dưới dạng table danh sách nhân viên
-    let tbody = getID('tableDanhSach');
+    // gọi hàm renderTable để hiển thị danh sách nhân viên
+    renderTable(staffList);
 
     // trước khi in ra danh sách nhân viên phải kiểm tra tất cả các field xem dữ liệu đã nhập có hợp lệ hay không
-    if (position === '0') {
-        alert('Vui lòng chọn chức vụ.');
-    }
-    else if (!checkAcc) {
-        queSel('#tbTKNV').innerHTML = 'Tài khoản phải được nhập bằng số.';
-    }
-    else if (String(account).length < 4 || String(account).length > 6) {
-        queSel('#tbTKNV').innerHTML = 'Tài khoản phải nhập từ 4 đến 6 ký số.';
-    }
-    else if (!checkName) {
-        queSel('#tbTen').innerHTML = 'Tên nhân viên phải là chữ.';
-    }
-    else if (!checkEmail) {
-        queSel('#tbEmail').innerHTML = 'Email phải đúng định dạng.';
-    }
-    else {
-        tbody.innerHTML += tr;
-    }
+    // if (position === '0') {
+    //     alert('Vui lòng chọn chức vụ.');
+    // }
+    // else if (!checkAcc) {
+    //     alert('Tài khoản phải được nhập bằng số.');
+    // }
+    // else if (!checkLength) {
+    //     alert('Tài khoản phải nhập từ 4 đến 6 ký số.');
+    // }
+    // else if (!checkName) {
+    //     alert('Tên nhân viên phải là chữ.');
+    // }
+    // else if (!checkEmail) {
+    //     alert('Email phải đúng định dạng.');
+    // }
+    // else {
+    //     // gọi hàm renderTable để hiển thị danh sách nhân viên
+    //     renderTable(staffList);
+    // }
 });
 
+// Tạo hàm hiển thị danh sách sinh viên ra table
+renderTable = (staffList) => {
+    let html = '';
+    for (let i = 0; i < staffList.length; i++) {
+        let staff = staffList[i];
+        html +=
+        `
+        <tr>
+            <td>${staff.account}</td>
+            <td>${staff.fullName}</td>
+            <td>${staff.email}</td>
+            <td>${staff.date}</td>
+            <td>${staff.position}</td>
+            <td>${staff.totalSalary()}</td>
+            <td>${staff.category()}</td>
+            <td><button type="button" class="btn btn-danger btnDelete">Delete</button></td>
+        </tr>
+        `;
+    }
+
+    getEle('#tableDanhSach').innerHTML = html;
+}
+
+
 // Tạo hàm kiểm tra tài khoản
-validateAcc = (acc) => {
+function validateAcc(acc) {
     let numbers = /^[0-9]+$/;
 
-    getID('tbTKNV').style.display = 'block';
+    getEle('#tbTKNV').style.display = 'block';
 
     if (acc.match(numbers)) {
+        getEle('#tbTKNV').innerHTML = '';
         return true;
     }
     else {
+        getEle('#tbTKNV').innerHTML = 'Tài khoản phải được nhập bằng số.';
         return false;
     }
 }
 
 
 // Tạo hàm kiểm tra họ tên
-validateName = (name) => {
+function validateName(name) {
     let letters = new RegExp("^[A-Za-z]+$");
 
-    getID('tbTen').style.display = 'block';
+    getEle('#tbTen').style.display = 'block';
 
     if (letters.test(name)) {
+        getEle('#tbTen').innerHTML = '';
         return true;
     }
     else {
+        getEle('#tbTen').innerHTML = 'Tên nhân viên phải là chữ.';
         return false;
     }
 }
 
 
 // Tạo hàm kiểm tra email
-validateEmail = (email) => {
+function validateEmail(email) {
     let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    getID('tbEmail').style.display = 'block';
+    getEle('#tbEmail').style.display = 'block';
 
     if (email.match(mailFormat)) {
+        getEle('#tbEmail').innerHTML = '';
         return true;
     }
     else {
+        getEle('#tbEmail').innerHTML = 'Email phải đúng định dạng.';
         return false;
     }
 }
+
+// Tạo hàm kiểm tra độ dài chuỗi
+function validateLength(input, min, max){
+    if (input.length >= min && input.length <= max) {
+        getEle('#tbTKNV').innerHTML = '';
+        return true;
+    } 
+    else {
+        getEle('#tbTKNV').innerHTML = 'Tài khoản phải nhập từ ' + min + ' đến ' + max + ' ký số.';
+        return false;
+    }
+}
+
+
+function getEle(selector) {
+    return document.querySelector(selector);
+}
+
+// console.log(staffList);
--- js/staff-info.js
+++ js/staff-info.js
@@ -1,44 +1,54 @@
 // Tạo lớp đối tượng nhân viên
-function StaffInfo(_account, _fullName, _email, _password, _date, _baseSalary, _position, _hours) {
+function StaffInfo(
+    account,
+    fullName,
+    email,
+    password,
+    date,
+    baseSalary,
+    position,
+    hours
+) {
     // property
-    this.account = _account;
-    this.fullName = _fullName;
-    this.email = _email;
-    this.password = _password;
-    this.date = _date;
-    this.baseSalary = _baseSalary;
-    this.position = _position;
-    this.hours = _hours;
+    this.account = account;
+    this.fullName = fullName;
+    this.email = email;
+    this.password = password;
+    this.date = date;
+    this.baseSalary = baseSalary;
+    this.position = position;
+    this.hours = hours;
+}
 
+// method
+StaffInfo.prototype.totalSalary = function () {
+    // tính tổng lương trong tháng dựa theo chức vụ
+    switch (this.position) {
+        case 'A':
+            return this.baseSalary * 3;
+        case 'B':
+            return this.baseSalary * 2;
+        case 'C':
+            return this.baseSalary;
+        default:
+            break;
+    }
+}
 
-    // method
-    this.totalSalary = function () { // tính tổng lương trong tháng
-        switch (this.position) {
-            case 'A':
-                return this.baseSalary * 3;
-            case 'B':
-                return this.baseSalary * 2;
-            case 'C':
-                return this.baseSalary;
-            default:
-                break;
-        }
-    };
-
-    this.category = function () { // xếp loại nhân viên dựa theo số giờ làm trong tháng
-        let result = '';
-        if (this.hours >= 192) {
-            result = 'Xuất sắc';
-        } 
-        else if (this.hours >= 176 && this.hours < 192) {
-            result = 'Giỏi';
-        }
-        else if (this.hours >= 160 && this.hours < 176){
-            result = 'Khá';
-        }
-        else if (this.hours < 160){
-            result = 'Trung bình';
-        }
-        return result;
-    };
+StaffInfo.prototype.category = function () {
+    // xếp loại nhân viên dựa theo số giờ làm trong tháng
+    let result = '';
+    if (this.hours >= 192) {
+        result = 'Xuất sắc';
+    }
+    else if (this.hours >= 176) {
+        result = 'Giỏi';
+    }
+    else if (this.hours >= 160) {
+        result = 'Khá';
+    }
+    else if (this.hours < 160) {
+        result = 'Trung bình';
+    }
+    return result;
 }
